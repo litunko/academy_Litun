3df186e4a38e922eac3dc8b87c0d536a08a06da5
iskutar <i.skutar@svitla.com>
2014-10-26 08:47:05 +0200

    comments to db

---
+++ comments_university.sql
@@ -0,0 +1,174 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.1
+-- http://www.phpmyadmin.net
+--
+-- Хост: 127.0.0.1
+-- Время создания: Окт 25 2014 г., 15:20
+-- Версия сервера: 5.6.19
+-- Версия PHP: 5.2.12
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- База данных: `university`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `courses`
+--
+
+CREATE TABLE IF NOT EXISTS `courses` (
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `name` char(50) NOT NULL,
+  `semester` int(20) NOT NULL,
+  `final_control` char(50) NOT NULL,
+  `faculty_id` int(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `faculty_id` (`faculty_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `course_teachers`
+--
+
+CREATE TABLE IF NOT EXISTS `course_teachers` ( -- courses_teachers, бо звязок багато до багато
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `course_id` int(20) NOT NULL,
+  `teacher_id` int(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `course_id` (`course_id`),
+  KEY `teacher_id` (`teacher_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `degree`
+--
+
+CREATE TABLE IF NOT EXISTS `degree` ( -- degrees
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `name` char(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `faculties`
+--
+
+CREATE TABLE IF NOT EXISTS `faculties` (
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `name` char(50) NOT NULL,
+  `adress` char(50) NOT NULL,
+  `year` int(4) NOT NULL, -- э тип поля date->year
+  `name_dekan` char(50) NOT NULL, -- не name, а звязок на таблицю teachers
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `groups`
+--
+
+CREATE TABLE IF NOT EXISTS `groups` (
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `name` char(50) NOT NULL,
+  `number_students` int(20) NOT NULL,
+  `name_leader` char(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `group_courses`
+--
+
+CREATE TABLE IF NOT EXISTS `group_courses` ( --groups_courses
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `group_id` int(20) NOT NULL,
+  `course_id` int(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `group_id` (`group_id`),
+  KEY `course_id` (`course_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `post`
+--
+
+CREATE TABLE IF NOT EXISTS `post` (
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `name` char(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `teachers`
+--
+
+CREATE TABLE IF NOT EXISTS `teachers` (
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `first_name` char(50) NOT NULL,
+  `second_name` char(50) NOT NULL,
+  `date` char(50) NOT NULL, --що за char?
+  `post_id` int(20) NOT NULL,
+  `degree_id` int(20) NOT NULL,
+  `topic` char(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `post_id` (`post_id`),
+  KEY `degree_id` (`degree_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `courses`
+--
+ALTER TABLE `courses`
+  ADD CONSTRAINT `courses_ibfk_1` FOREIGN KEY (`faculty_id`) REFERENCES `faculties` (`id`);
+
+--
+-- Ограничения внешнего ключа таблицы `course_teachers`
+--
+ALTER TABLE `course_teachers`
+  ADD CONSTRAINT `course_teachers_ibfk_2` FOREIGN KEY (`teacher_id`) REFERENCES `teachers` (`id`),
+  ADD CONSTRAINT `course_teachers_ibfk_1` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`);
+
+--
+-- Ограничения внешнего ключа таблицы `group_courses`
+--
+ALTER TABLE `group_courses`
+  ADD CONSTRAINT `group_courses_ibfk_2` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`),
+  ADD CONSTRAINT `group_courses_ibfk_1` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`);
+
+--
+-- Ограничения внешнего ключа таблицы `teachers`
+--
+ALTER TABLE `teachers`
+  ADD CONSTRAINT `teachers_ibfk_2` FOREIGN KEY (`degree_id`) REFERENCES `degree` (`id`),
+  ADD CONSTRAINT `teachers_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `post` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
